4428d3c817558400eaaee30da95a98ed1041ae34
Akaikenlol <keithken972@gmail.com>
2023-12-23 21:35:54 +0630

    feat: NavBar style: add icons, global css and tailwind config

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 export const metadata: Metadata = {
 	title: "Hilink",
@@ -13,7 +15,12 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="en">
-			<body>{children}</body>
+			<body>
+				<Navbar />
+				<main className="relative overflow-hidden">{children}</main>
+
+				<Footer />
+			</body>
 		</html>
 	);
 }
